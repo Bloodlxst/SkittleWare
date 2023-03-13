90041f2b8c3cd8c31c56a66abba374dea3aff426
Bloodlxst <87296811+Bloodlxst@users.noreply.github.com>
2023-03-12 22:11:56 -0400

    Update Star.lua

--- Misc/Star.lua
+++ Misc/Star.lua
@@ -1,27 +1,27 @@
- coroutine.resume(coroutine.create(function()
-        while wait(0) do
-        local success, err = pcall(function()
-            for i,v in pairs(game.Players:GetChildren()) do
-                if v.Character then
-                    if table.find(Owner,v.UserId) then
-                        v.Character.Humanoid.DisplayName = "[🔨👑📜💎] "..v.DisplayName 
-                    end
-                end
-            end
-        end)
-        end
-    end))
-      
-    coroutine.resume(coroutine.create(function()
-        while wait(5) do
-        local success, err = pcall(function()
-            for i,v in pairs(game.Players:GetChildren()) do
-                if v.Character then
-                    if table.find(Premium,v.UserId) then
-                        v.Character.Humanoid.DisplayName = "[🎭] "..v.DisplayName
-                    end
-                end
-            end
-        end)
-        end
-    end))
+coroutine.resume(coroutine.create(function()
+while wait(0.1) do
+local success, err = pcall(function()
+for i,v in pairs(game.Players:GetChildren()) do
+	if v.Character then
+		if table.find(Owner,v.UserId) then
+			v.Character.Humanoid.DisplayName = "[📜💎] "..v.DisplayName 
+		end
+	end
+end
+end)
+end
+end))
+
+coroutine.resume(coroutine.create(function()
+while wait(0.1) do
+local success, err = pcall(function()
+for i,v in pairs(game.Players:GetChildren()) do
+	if v.Character and not table.find(Owner, game:GetService("Players").LocalPlayer.UserId) then
+		if table.find(Premium,v.UserId) then
+			v.Character.Humanoid.DisplayName = "[😎] "..v.DisplayName
+		end
+	end
+end 
+end)
+end
+end))
